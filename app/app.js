ef229577379548c737472a820a818500d22c79ac
Qblack <blac2410@mylaurier.ca>
2015-04-01 22:46:44 -0400

    added username to cookie

--- app/app.js
+++ app/app.js
@@ -7,7 +7,8 @@ angular.module('transcoding-ui', [
   'ngCookies',
   'transcoding-ui.directives',
   'transcoding-ui.view_form',
-  'transcoding-ui.view_history'
+  'transcoding-ui.view_history',
+  'transcoding-ui.view_login'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view_form'});
